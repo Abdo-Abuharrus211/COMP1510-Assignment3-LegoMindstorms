8c9450c818c23888c1a411ddb1cd1b61c2170ff1
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-03-02 13:28:14 -0800

    Initial commit

---
+++ Line follow/line_follow_v1.py
@@ -0,0 +1,15 @@
+from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor, App
+from mindstorms.control import wait_for_seconds, wait_until, Timer
+from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
+import math
+
+hub = MSHub()
+movement_motors = MotorPair('B', 'A')
+color_sensor = ColorSensor('E')
+
+
+hub.speaker.beep()
+
+
+while True:
+    movement_motors.start((color_sensor.get_reflected_light()-50)*3, 11)
---
+++ pycharm experimental/proportional_line_follow.py
@@ -0,0 +1,21 @@
+from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor, App
+from mindstorms.control import wait_for_seconds, wait_until, Timer
+from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, \
+    not_equal_to
+import math
+
+hub = MSHub()
+movement_motors = MotorPair('B', 'A')
+color_sensor = ColorSensor('E')
+
+hub.speaker.beep()
+
+# black value
+target_path = 10
+# steering const
+prop_const = 2
+movement_speed = set_default_speed(30)
+
+# infinite loop
+while True:
+    movement_motors.start((prop_const * (color_sensor.get_reflected_light() - target_path)), movement_speed)
